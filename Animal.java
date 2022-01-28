a53678b3cf7d1d4f9b6bbff1dfcd5e9aeedda2ae
Favionata16 <98546416+Favionata16@users.noreply.github.com>
2022-01-28 14:51:04 -0600

    Add files via upload

--- Animal.java
+++ Animal.java
@@ -7,8 +7,91 @@ package TcLab;
 
 /**
  *
- * @author daniel
+ * @author favio
  */
 public class Animal {
+    private String nombreCientifico;
+    private String nombreComun;
+    private String habitat;
+    private String alimentacion;
+    private String rasgos;
+    private String geografica;
+    private int vida;
+    
+    public Animal() {
+    }
+
+
+    public Animal(String nombreCientifico, String nombreComun, String habitat, String alimentacion, String rasgos, String geografica, int vida) {
+        this.nombreCientifico = nombreCientifico;
+        this.nombreComun = nombreComun;
+        this.habitat = habitat;
+        this.alimentacion = alimentacion;
+        this.rasgos = rasgos;
+        this.geografica = geografica;
+        this.vida = vida;
+    }
+
+    public String getNombreCientifico() {
+        return nombreCientifico;
+    }
+
+    public void setNombreCientifico(String nombreCientifico) {
+        this.nombreCientifico = nombreCientifico;
+    }
+
+    public String getNombreComun() {
+        return nombreComun;
+    }
+
+    public void setNombreComun(String nombreComun) {
+        this.nombreComun = nombreComun;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public String getAlimentacion() {
+        return alimentacion;
+    }
+
+    public void setAlimentacion(String alimentacion) {
+        this.alimentacion = alimentacion;
+    }
+
+    public String getRasgos() {
+        return rasgos;
+    }
+
+    public void setRasgos(String rasgos) {
+        this.rasgos = rasgos;
+    }
+
+    public String getGeografica() {
+        return geografica;
+    }
+
+    public void setGeografica(String geografica) {
+        this.geografica = geografica;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    @Override
+    public String toString() {
+        return "Animales" + "Nombre Cientifico: " + nombreCientifico + ", Nombre comun: " + nombreComun + ", habitat" + habitat + ", alimentacion" + alimentacion + ", rasgos" + rasgos + ", geografia" + geografica + ", vida" + vida;
+    }
+    
     
 }
