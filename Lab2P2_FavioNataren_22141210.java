a4d33913bc48d70a247334d70ade27080575c2e6
Favionata16 <98546416+Favionata16@users.noreply.github.com>
2022-01-28 13:32:00 -0600

    Add files via upload

---
+++ Lab2P2_FavioNataren_22141210.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TcLab;
+
+/**
+ *
+ * @author favio
+ */
+public class Lab2P2_FavioNataren_22141210 {
+
+}
