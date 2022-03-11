07417037a7e51b635e87197c2a18d9952cc23697
Favionata16 <98546416+Favionata16@users.noreply.github.com>
2022-03-11 15:22:54 -0600

    gg

---
+++ VideoTutorialesJava/src/videotutorialesjava/main.java
@@ -0,0 +1,21 @@
+package videotutorialesjava;
+
+import Datos.Conexion;
+
+/**
+ *
+ * @author mrrecursos.com
+ */
+public class main {
+    
+    public static void main(String[] args) {
+       new Principal().setVisible(true);            
+        /*Conexion c = new Conexion();
+        if(c.getConexion()!=null)
+            System.out.println("Conexión correcta");
+        else
+            System.out.println("Conexión errónea");*/       
+        
+    }
+    
+}
